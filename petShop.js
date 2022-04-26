1a6328b9efc3d298c6cd109ecb12e202a222f505
GalkaKG <103485495+GalkaKG@users.noreply.github.com>
2022-04-26 16:13:42 +0300

    Create petShop.js

---
+++ petShop.js
@@ -0,0 +1,7 @@
+function petShop (input) {
+   let numForFoodDogs = input[0];
+   let numForFoodCats = input[1];
+   let sum = (numForFoodDogs * 2.50) + (numForFoodCats * 4);
+  
+   console.log(`${sum} lv. `);
+}
